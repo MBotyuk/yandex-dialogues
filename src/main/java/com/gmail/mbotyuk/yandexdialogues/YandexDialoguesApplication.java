d8233c4a47b5d524c7ebcb0fee619b8dc98b3ed7
MBotyuk <mbotyuk@gmail.com>
2024-02-18 10:09:45 +0300

    init commit

---
+++ src/main/java/com/gmail/mbotyuk/yandexdialogues/YandexDialoguesApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.mbotyuk.yandexdialogues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexDialoguesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YandexDialoguesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmail/mbotyuk/yandexdialogues/YandexDialoguesApplicationTests.java
@@ -0,0 +1,15 @@
+package com.gmail.mbotyuk.yandexdialogues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test") //активировать тестовый профиль Spring для тестов
+class YandexDialoguesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
